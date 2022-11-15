13181d2fdd11b6c49a99b122efb575b90c7780bf
Ricky <anandaricky01@gmail.com>
2022-11-15 16:34:39 +0700

    adding save post

--- app/models/post.model.js
+++ app/models/post.model.js
@@ -2,7 +2,8 @@ module.exports = (mongoose) => {
     const Schema = mongoose.Schema({
         title : {
             type : String,
-            required : true
+            required : true,
+            unique : true
         },
         slug : {
             type : String,
@@ -20,6 +21,9 @@ module.exports = (mongoose) => {
             type : Date,
             default : Date.now
         }
+    },
+    {
+      timestamps: true,
     });
 
     const Post = mongoose.model("posts", Schema);
